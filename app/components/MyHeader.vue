674a0b180b366d0dea25dd45e11dfd66b9bcbb18
Matthis Boreaniz <matthisonepiece@gmail.com>
2025-12-02 09:36:35 +0100

    feat/header add Html header

---
+++ app/components/MyHeader.vue
@@ -0,0 +1,32 @@
+<script setup mang="ts"></script>
+
+<template>
+  <div class="header">
+    <ul>
+      <li>Community</li>
+      <li>Books</li>
+      <li>Recipe Index</li>
+      <li>Popular</li>
+    </ul>
+    <ul>
+      <li><img src="" alt="logout" />Register</li>
+      <li><img src="" alt="usericon" />Login</li>
+    </ul>
+  </div>
+
+  <div class="header__under">
+    <img class="logo" src="" alt="LogoLarge" />
+    <p>searchBar</p>
+    <img src="" alt="panier" />
+    <div>
+      <img src="" alt="pfp" />
+      <ul>
+        <li>Profile</li>
+        <li>Privacy</li>
+        <li>Collections</li>
+        <li>Settings</li>
+        <li>Logout</li>
+      </ul>
+    </div>
+  </div>
+</template>
